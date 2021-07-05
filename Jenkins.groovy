dbdaec6bfa4009a640841d6cc131cc61c2ff4ce1
sivakumari868 <kumari6.siva@gmail.com>
2021-07-05 06:24:08 -0400

    adding new file

---
+++ Jenkins.groovy
@@ -0,0 +1,20 @@
+pipeliine {
+    stages {
+        stage('Checkout') {
+            checkout scm
+        }
+        stage('Validate the code'){
+            script {
+                echo 'Validated the test cases'
+            }
+        }
+        stage('Deploy the code') {
+            script {
+                sh '''
+                    sudo cp -pr . /var/www/html/
+                    sudo service httpd restart
+                '''
+            }
+        }
+    }
+}
